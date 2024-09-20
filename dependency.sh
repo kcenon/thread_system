cbc23e621a1b8e170527b38f3cced487eaf3df24
kcenon <kcenon@naver.com>
2024-09-21 03:06:29 +0900

    Created thread base

---
+++ dependency.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+    brew update
+    brew upgrade
+    brew install pkg-config cmake doxygen
+elif [ "$(uname)" == "Linux" ]; then
+    apt update
+    apt upgrade -y
+
+    apt install cmake build-essential gdb doxygen -y
+
+    apt-get install curl zip unzip tar -y
+    apt-get install pkg-config -y
+
+    if [ $(uname -m) == "aarch64" ]; then
+        export VCPKG_FORCE_SYSTEM_BINARIES=arm
+    fi
+fi
+
+doxygen
+
+cd ..
+
+if [ ! -d "./vcpkg/" ]; then
+    git clone https://github.com/microsoft/vcpkg.git
+fi
+
+cd vcpkg
+
+git pull
+./bootstrap-vcpkg.sh
+
+cd ..
