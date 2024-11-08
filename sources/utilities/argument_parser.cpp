29d023f7008cb32239a0dce79636c9a08961e82b
kcenon <kcenon@naver.com>
2024-11-09 08:23:17 +0900

    Update argument_parser

--- sources/utilities/argument_parser.cpp
+++ sources/utilities/argument_parser.cpp
@@ -32,39 +32,39 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "argument_parser.h"
 
-#include <charconv>
+#include "convert_string.h"
+
 #include <algorithm>
 
 namespace utility_module
 {
-	argument_manager::argument_manager(void) {}
+
+	argument_manager::argument_manager() {}
 
 	auto argument_manager::to_string(const std::string& key) -> std::optional<std::string>
 	{
-		auto target = arguments_.find(key);
-		return target != arguments_.end() ? std::optional(target->second) : std::nullopt;
+		auto it = arguments_.find(key);
+		return it != arguments_.end() ? std::optional(it->second) : std::nullopt;
 	}
 
 	auto argument_manager::to_bool(const std::string& key) -> std::optional<bool>
 	{
-		auto target = to_string(key);
-		if (!target.has_value())
+		auto value = to_string(key);
+		if (!value.has_value())
 		{
 			return std::nullopt;
 		}
 
-		auto temp = target.value();
+		auto temp = value.value();
 		std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
 		if (temp == "true" || temp == "1")
 		{
 			return true;
 		}
-
 		if (temp == "false" || temp == "0")
 		{
 			return false;
 		}
-
 		return std::nullopt;
 	}
 
@@ -100,6 +100,180 @@ namespace utility_module
 	}
 #endif
 
+	template <typename NumericType>
+	auto argument_manager::to_numeric(const std::string& key) -> std::optional<NumericType>
+	{
+		auto value = to_string(key);
+		if (!value.has_value())
+		{
+			return std::nullopt;
+		}
+
+		try
+		{
+			if constexpr (std::is_same_v<NumericType, int>)
+			{
+				return std::stoi(value.value());
+			}
+			else if constexpr (std::is_same_v<NumericType, long>)
+			{
+				return std::stol(value.value());
+			}
+			else if constexpr (std::is_same_v<NumericType, long long>)
+			{
+				return std::stoll(value.value());
+			}
+			else if constexpr (std::is_same_v<NumericType, unsigned long>)
+			{
+				return std::stoul(value.value());
+			}
+			else if constexpr (std::is_same_v<NumericType, unsigned long long>)
+			{
+				return std::stoull(value.value());
+			}
+			else if constexpr (std::is_same_v<NumericType, short>)
+			{
+				return static_cast<short>(std::stoi(value.value()));
+			}
+			else if constexpr (std::is_same_v<NumericType, unsigned short>)
+			{
+				return static_cast<unsigned short>(std::stoul(value.value()));
+			}
+			else if constexpr (std::is_same_v<NumericType, unsigned int>)
+			{
+				return static_cast<unsigned int>(std::stoul(value.value()));
+			}
+		}
+		catch (...)
+		{
+			return std::nullopt;
+		}
+		return std::nullopt;
+	}
+
+	template <typename StringType>
+	auto argument_manager::try_parse(const StringType& arguments)
+		-> std::tuple<bool, std::optional<std::string>>
+	{
+		if (arguments.empty())
+		{
+			return { false, "no valid arguments found." };
+		}
+
+		std::optional<std::string> converted;
+		std::optional<std::string> convert_error;
+
+		if constexpr (std::is_same_v<StringType, std::string>
+					  || std::is_same_v<StringType, const char*>)
+		{
+			converted = std::string(arguments);
+		}
+		else if constexpr (std::is_same_v<StringType, std::wstring>
+						   || std::is_same_v<StringType, const wchar_t*>)
+		{
+			std::tie(converted, convert_error) = convert_string::to_string(std::wstring(arguments));
+		}
+		else
+		{
+			return { false, "Unsupported string type" };
+		}
+
+		if (!converted.has_value())
+		{
+			return { false, convert_error };
+		}
+
+		auto argument_string = converted.value();
+
+		bool only_whitespace
+			= std::all_of(argument_string.begin(), argument_string.end(),
+						  [](unsigned char c) { return std::isspace(c) || c == '\0'; });
+		if (only_whitespace)
+		{
+			return { false, "no valid arguments found." };
+		}
+
+		// null 문자 처리
+		auto null_pos = argument_string.find('\0');
+		if (null_pos != std::string::npos)
+		{
+			argument_string = argument_string.substr(0, null_pos);
+		}
+
+		auto [splitted, split_error] = convert_string::split(argument_string, " ");
+		if (!splitted.has_value())
+		{
+			return { false, split_error };
+		}
+
+		auto splitted_vector = splitted.value();
+		splitted_vector.erase(
+			std::remove_if(
+				splitted_vector.begin(), splitted_vector.end(), [](const std::string& s)
+				{ return s.empty() || s.find_first_not_of(" \t\n\r\0") == std::string::npos; }),
+			splitted_vector.end());
+
+		if (splitted_vector.empty())
+		{
+			return { false, "no valid arguments found." };
+		}
+
+		auto [parsed, parse_error] = parse(splitted_vector);
+		if (!parsed.has_value())
+		{
+			return { false, parse_error };
+		}
+
+		arguments_ = parsed.value();
+		return { true, std::nullopt };
+	}
+
+	template <typename CharType>
+	auto argument_manager::try_parse(int argc, CharType* argv[])
+		-> std::tuple<bool, std::optional<std::string>>
+	{
+		std::vector<std::string> args;
+		bool found_valid = false;
+
+		for (int i = 0; i < argc; ++i)
+		{
+			std::string arg;
+			if constexpr (std::is_same_v<CharType, wchar_t>)
+			{
+				auto [converted, error] = convert_string::to_string(std::wstring(argv[i]));
+				if (!converted.has_value())
+				{
+					return { false, error };
+				}
+				arg = converted.value();
+			}
+			else
+			{
+				arg = std::string(argv[i]);
+			}
+
+			if (!arg.empty() && arg.substr(0, 2) == "--")
+			{
+				found_valid = true;
+			}
+			args.push_back(arg);
+		}
+
+		if (!found_valid)
+		{
+			return { false, "no valid arguments found." };
+		}
+
+		auto [parsed, error] = parse(args);
+		if (!parsed.has_value())
+		{
+			return { false, error };
+		}
+
+		arguments_ = parsed.value();
+		return { true, std::nullopt };
+	}
+
 	auto argument_manager::parse(const std::vector<std::string>& arguments)
 		-> std::tuple<std::optional<std::map<std::string, std::string>>, std::optional<std::string>>
 	{
@@ -108,10 +282,25 @@ namespace utility_module
 			return { std::nullopt, "no valid arguments found." };
 		}
 
-		std::map<std::string, std::string> result;
-		bool found_valid_argument = false;
+		if (arguments[0].substr(0, 2) != "--")
+		{
+			if (arguments.size() == 1)
+			{
+				return { std::nullopt, "invalid argument: " + arguments[0] };
+			}
 
-		for (size_t i = 0; i < arguments.size(); ++i)
+			if (arguments[1].substr(0, 2) != "--")
+			{
+				return { std::nullopt, "invalid argument: " + arguments[1] };
+			}
+		}
+
+		std::map<std::string, std::string> result;
+		bool found_valid = false;
+
+		size_t i = (arguments[0].substr(0, 2) != "--") ? 1 : 0;
+
+		for (; i < arguments.size(); ++i)
 		{
 			const auto& arg = arguments[i];
 
@@ -122,8 +311,8 @@ namespace utility_module
 
 			if (arg == "--help")
 			{
-				result.insert({ arg, "display help" });
-				found_valid_argument = true;
+				result[arg] = "display help";
+				found_valid = true;
 				continue;
 			}
 
@@ -132,20 +321,33 @@ namespace utility_module
 				return { std::nullopt, "argument '" + arg + "' expects a value." };
 			}
 
-			if (arguments[i + 1].substr(0, 2) == "--")
+			const auto& next_arg = arguments[i + 1];
+			if (next_arg.substr(0, 2) == "--")
 			{
 				return { std::nullopt, "argument '" + arg + "' expects a value." };
 			}
 
-			result[arg] = arguments[++i];
-			found_valid_argument = true;
+			result[arg] = next_arg;
+			found_valid = true;
+			++i;
 		}
 
-		if (!found_valid_argument)
+		if (!found_valid)
 		{
 			return { std::nullopt, "no valid arguments found." };
 		}
 
 		return { result, std::nullopt };
 	}
-} // namespace argument_parser
+
+	// Explicit template instantiations
+	template auto argument_manager::try_parse<std::string>(const std::string&)
+		-> std::tuple<bool, std::optional<std::string>>;
+	template auto argument_manager::try_parse<std::wstring>(const std::wstring&)
+		-> std::tuple<bool, std::optional<std::string>>;
+	template auto argument_manager::try_parse<char>(int, char*[])
+		-> std::tuple<bool, std::optional<std::string>>;
+	template auto argument_manager::try_parse<wchar_t>(int, wchar_t*[])
+		-> std::tuple<bool, std::optional<std::string>>;
+
+} // namespace utility_module
