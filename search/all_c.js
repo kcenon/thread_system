91d5ffd63095c52cdd4ad6eb7eaca198bc08c4b5
kcenon <kcenon@users.noreply.github.com>
2025-03-13 14:41:14 +0000

    Update documentation via GitHub Actions f1f7f8933769cc98ca1a08abf70517ec7c880530

--- search/all_c.js
+++ search/all_c.js
@@ -4,6 +4,6 @@ var searchData=
   ['nanoseconds_1',['nanoseconds',['../db/d61/classutility__module_1_1datetime__tool.html#a715b310f2e28321265f02b39ef334eb6',1,'utility_module::datetime_tool']]],
   ['none_2',['None',['../d9/de0/namespacelog__module.html#ac59af6ec9704b7fd0d73e7becbc54709a6adf97f83acf6453d4a6a4b1070f3754',1,'log_module']]],
   ['normal_3',['Normal',['../d8/da5/namespacepriority__thread__pool__module.html#ad02023113ce2026fd0599170b3aad090a960b44c579bc2f6818d2daaf9e4c16f0',1,'priority_thread_pool_module']]],
-  ['note_4',['Note',['../d4/df8/classpriority__thread__pool__module_1_1priority__thread__pool__t.html#autotoc_md7',1,'']]],
+  ['note_4',['note',['../d4/df8/classpriority__thread__pool__module_1_1priority__thread__pool__t.html#autotoc_md8',1,'Note'],['../d4/df8/classpriority__thread__pool__module_1_1priority__thread__pool__t.html#autotoc_md10',1,'Note']]],
   ['notify_5f_5',['notify_',['../df/d6d/classthread__module_1_1job__queue.html#a3d291457b201a92d3ed9b0761b00c133',1,'thread_module::job_queue']]]
 ];
