8f82f0e80b1253b2a3cf787a71100700d8ec9be3
kcenon <kcenon@users.noreply.github.com>
2025-05-25 02:53:59 +0000

    Update documentation via GitHub Actions 4cc267df2ea6b22dc21a774dc946e76898c8779a

--- search/files_6.js
+++ search/files_6.js
@@ -5,7 +5,7 @@ var searchData=
   ['log_5fjob_2ecpp_2',['log_job.cpp',['../d8/de3/log__job_8cpp.html',1,'']]],
   ['log_5fjob_2eh_3',['log_job.h',['../d1/dc2/log__job_8h.html',1,'']]],
   ['log_5ftypes_2eh_4',['log_types.h',['../d1/d18/log__types_8h.html',1,'']]],
-  ['logger_2eh_5',['logger.h',['../d1/d8c/logger_8h.html',1,'']]],
+  ['logger_2eh_5',['logger.h',['../de/d39/logger_2logger_8h.html',1,'(Global Namespace)'],['../de/dd3/utilities_2logger_8h.html',1,'(Global Namespace)']]],
   ['logger_5fimplementation_2ecpp_6',['logger_implementation.cpp',['../d9/d7e/logger__implementation_8cpp.html',1,'']]],
   ['logger_5fimplementation_2eh_7',['logger_implementation.h',['../d5/d14/logger__implementation_8h.html',1,'']]]
 ];
