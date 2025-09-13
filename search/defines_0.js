7723d55fbd19b5dbd822d85abdc572b1f12f148c
kcenon <kcenon@users.noreply.github.com>
2025-09-13 05:38:48 +0000

    Update documentation via GitHub Actions f58086efea3060427293136f6335cd6bad04c652

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['declare_5fformatter_0',['DECLARE_FORMATTER',['../d2/d8a/formatter__macros_8h.html#ad353e070b78b0b1f98da791dfc46f0a1',1,'formatter_macros.h']]]
+  ['declare_5fconditional_5fformatter_0',['DECLARE_CONDITIONAL_FORMATTER',['../d2/d8a/formatter__macros_8h.html#a29883d06f1fef46524299dfac3ebc518',1,'formatter_macros.h']]],
+  ['declare_5fformatter_1',['DECLARE_FORMATTER',['../d2/d8a/formatter__macros_8h.html#ad353e070b78b0b1f98da791dfc46f0a1',1,'formatter_macros.h']]],
+  ['declare_5fformatter_5fwith_5ffunction_2',['DECLARE_FORMATTER_WITH_FUNCTION',['../d2/d8a/formatter__macros_8h.html#a89e5c8537c526ebf37cf441cd87296a1',1,'formatter_macros.h']]]
 ];
