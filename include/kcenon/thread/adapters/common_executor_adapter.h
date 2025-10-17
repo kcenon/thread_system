39e7031ce8bcbe3931311328ade0d274a2c15be1
kcenon <kcenon@naver.com>
2025-10-17 15:58:55 +0900

    Update license years to 2025

--- include/kcenon/thread/adapters/common_executor_adapter.h
+++ include/kcenon/thread/adapters/common_executor_adapter.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
 BSD 3-Clause License
 
-Copyright (c) 2021, 🍀☀🌕🌥 🌊
+Copyright (c) 2025, 🍀☀🌕🌥 🌊
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
