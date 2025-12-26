a10dbcdf1f3086b1938973a419d4b29871b98330
kcenon <kcenon@users.noreply.github.com>
2025-12-26 23:31:44 +0000

    Update documentation via GitHub Actions c3255ea8201c97a00905ef87d740a96ae296ae9f

--- search/all_11.js
+++ search/all_11.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['quality_20assurance_0',['Quality Assurance',['../index.html#quality_assurance',1,'']]],
-  ['queue_5fempty_1',['queue_empty',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a64a7ed43d482ecf6f39282aaef0e4c02',1,'kcenon::thread']]],
-  ['queue_5ffull_2',['queue_full',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7a254d61c3058375b49dbabd86631b37',1,'kcenon::thread']]],
-  ['queue_5fstopped_3',['queue_stopped',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7cf957d8abdc60ec16fc5bd302c55987',1,'kcenon::thread']]],
-  ['quick_20start_20guide_4',['Quick Start Guide',['../index.html#quick_start',1,'']]]
+  ['queue_5fbusy_1',['queue_busy',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a00f1656f6fe1846eb54653bd597a9006',1,'kcenon::thread']]],
+  ['queue_5fempty_2',['queue_empty',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a64a7ed43d482ecf6f39282aaef0e4c02',1,'kcenon::thread']]],
+  ['queue_5ffull_3',['queue_full',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7a254d61c3058375b49dbabd86631b37',1,'kcenon::thread']]],
+  ['queue_5fstopped_4',['queue_stopped',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7cf957d8abdc60ec16fc5bd302c55987',1,'kcenon::thread']]],
+  ['quick_20start_20guide_5',['Quick Start Guide',['../index.html#quick_start',1,'']]]
 ];
--- search/enumvalues_a.js
+++ search/enumvalues_a.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['queue_5fempty_0',['queue_empty',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a64a7ed43d482ecf6f39282aaef0e4c02',1,'kcenon::thread']]],
-  ['queue_5ffull_1',['queue_full',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7a254d61c3058375b49dbabd86631b37',1,'kcenon::thread']]],
-  ['queue_5fstopped_2',['queue_stopped',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7cf957d8abdc60ec16fc5bd302c55987',1,'kcenon::thread']]]
+  ['queue_5fbusy_0',['queue_busy',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a00f1656f6fe1846eb54653bd597a9006',1,'kcenon::thread']]],
+  ['queue_5fempty_1',['queue_empty',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a64a7ed43d482ecf6f39282aaef0e4c02',1,'kcenon::thread']]],
+  ['queue_5ffull_2',['queue_full',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7a254d61c3058375b49dbabd86631b37',1,'kcenon::thread']]],
+  ['queue_5fstopped_3',['queue_stopped',['../da/dcc/namespacekcenon_1_1thread.html#a20e1d8b59344b87a5dc3a46603607a23a7cf957d8abdc60ec16fc5bd302c55987',1,'kcenon::thread']]]
 ];
